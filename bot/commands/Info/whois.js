785798291881f0a12137ee434e8d85f8c914481e
Kayz-xx <83003158+Kayz-xx@users.noreply.github.com>
2021-07-13 12:52:15 +0530

    Create whois.js

---
+++ bot/commands/Info/whois.js
@@ -0,0 +1,61 @@
+const Discord = require('discord.js');
+const moment = require('moment');
+
+module.exports = {
+	name: 'whois',
+	description: 'Get the info of any user',
+	category: 'info',
+	usage: '<id>',
+
+	async execute(client, message, cmd, args) {
+		let users = args[0];
+		if (!users)
+			return message.channel.send(
+				'```whois <userid>\n      ^^^^^^^^\nuserid is a required argument which is missing.```'
+			);
+
+			const member = await client.users.fetch(users) 
+
+			let inGuild = false
+   
+			if(message.guild.members.fetch(member.id)) inGuild = true 
+   
+   
+		   const embed = new Discord.MessageEmbed()
+			   .setAuthor(
+				   `${member.tag}`,
+				   member.avatarURL()
+			   )
+			   .setThumbnail(
+				   member.avatarURL()
+			   )
+			   .setDescription(`In Elites\n**${inGuild}**`)
+			   .addFields(
+				   {
+					   name: 'Joined Discord',
+					   value: `${moment(member.createdAt).format(
+						   'DD MMM YYYY'
+					   )}`,
+					   inline: true,
+				   },
+				   {
+					   name: 'Discriminator',
+					   value: `${member.discriminator}`,
+					   inline: true,
+				   },
+				   {
+					   name: 'Presence',
+					   value: `${member.presence.status}`,
+					   inline: true,
+				   },
+				   {
+					   name: 'Bot',
+					   value: `${member.bot}`,
+					   inline: true,
+				   },
+				   { name: 'User ID', value: `${member.id}`, inline: true },
+			   )
+			   .setColor(`RANDOM`);
+		   message.channel.send(embed);
+	   }
+   };
