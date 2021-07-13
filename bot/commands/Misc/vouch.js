545e495b74baec616f5703d9d2476dc02a3b014e
Kayz-xx <83003158+Kayz-xx@users.noreply.github.com>
2021-07-13 12:48:55 +0530

    Update vouch.js

--- bot/commands/Misc/vouch.js
+++ bot/commands/Misc/vouch.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 module.exports = {
 	name: 'vouch',
 	aliases: ['vc'],
-	cooldown: '5',
+	cooldown: '0',
 	permissions: [],
 	category: 'Misc',
 	usage: '<user>',
@@ -12,54 +12,96 @@ module.exports = {
 
 	async execute(client, message, cmd, args) {
 		if (cmd === 'vouch') {
-			if (!message.member.hasPermission('MANAGE_GUILD'))
-				return message.channel
-					.send('You do not have permission to use this command.')
-					.then((m) => m.delete({ timeout: 5000 }));
-			const target = message.mentions.users.first();
-			if (!target) {
-				message.reply('Please specify someone to vouch');
-				return;
+			if (message.channel.id === '863445906636537867') {
+				const target = message.mentions.members.first();
+				if (!target) {
+					message.reply('Please specify someone to vouch');
+					return;
+				}
+
+				const { guild } = message;
+				const guildId = guild.id;
+				const targetId = target.user.id;
+				const authorId = message.author.id;
+				const now = new Date();
+
+				if (targetId === authorId) {
+					message.reply('You cannot vouch yourself');
+					return;
+				}
+
+				const data =
+					(await db
+						.ref(`Vouch System/${message.guild.id}/${targetId}`)
+						.once('value')
+						.then((snapshot) => snapshot.val())) || [];
+
+				db.ref(`Vouch System/${message.guild.id}/${targetId}`);
+
+				const vouches = client.vouches; // client.vouches = new Map () in index.js
+
+				if (!vouches.has(targetId)) {
+					vouches.set(targetId, []);
+				}
+
+				const vouched = vouches.get(targetId);
+
+				const user_vouched = vouched.find(
+					(recent) => recent.LastGivenBy === authorId
+				);
+
+				if (user_vouched) {
+					if (Date.now() - user_vouched.LastGivenAt < 900000) {
+						return message.channel.send(
+							new Discord.MessageEmbed()
+								.setAuthor(
+									message.author.tag,
+									message.author.displayAvatarURL()
+								)
+								.setFooter(
+									`You can vouch the same person every 15 mins`
+								)
+								.setDescription(
+									`You can't vouch that person again! <:ee:863849610540089365>`
+								)
+								.setColor('FF0000')
+						);
+					}
+				}
+
+				vouched.push({
+					LastGivenBy: authorId,
+					LastGivenAt: Date.now(),
+				});
+
+				/*let array = [targetId]
+
+				 array.push({
+					"LastGivenBy": authorId,
+					"LastGivenAt": Date.now()
+				})
+
+				console.log(array)
+				console.log(array[0])*/
+
+				if (!data.Vouches) {
+					let index = 1;
+					db.ref(
+						`Vouch System/${message.guild.id}/${targetId}/Vouches`
+					).set(index);
+				} else {
+					let data2 = data.Vouches + 1;
+					db.ref(
+						`Vouch System/${message.guild.id}/${targetId}/Vouches`
+					).set(data2);
+				}
+
+				message.react(`<:tick:859297441466679326>`);
 			}
-
-			const { guild } = message;
-			const guildId = guild.id;
-			const targetId = target.id;
-			const authorId = message.author.id;
-			const now = new Date();
-
-			if (targetId === authorId) {
-				message.reply('You cannot vouch yourself');
-				return;
-			}
-
-			let data =
-				(await db
-					.ref(`Vouch System/${message.guild.id}/${targetId}`)
-					.once('value')
-					.then((snapshot) => snapshot.val())) || [];
-			db.ref(`Vouch System/${message.guild.id}/${targetId}`);
-
-			if (!data.Vouches) {
-				let index = 1;
-				db.ref(
-					`Vouch System/${message.guild.id}/${targetId}/Vouches`
-				).set(index);
-			} else {
-				let data2 = data.Vouches + 1;
-				db.ref(
-					`Vouch System/${message.guild.id}/${targetId}/Vouches`
-				).set(data2);
-			}
-
-			message.react(`<:tick:859297441466679326>`);
 		}
+
 		if (cmd === 'vc') {
-			if (!message.member.hasPermission('MANAGE_GUILD'))
-				return message.channel
-					.send('You do not have permission to use this command.')
-					.then((m) => m.delete({ timeout: 5000 }));
-			const target = message.mentions.users.first() || message.author;
+			const target = message.mentions.members.first() || message.member;
 			if (!target) {
 				message.reply('Please specify a user!');
 				return;
@@ -67,7 +109,7 @@ module.exports = {
 
 			const { guild } = message;
 			const guildId = guild.id;
-			const targetId = target.id;
+			const targetId = target.user.id;
 
 			let data =
 				(await db
@@ -79,30 +121,38 @@ module.exports = {
 			if (!data.Vouches) {
 				return message.channel.send({
 					embed: {
-						title: `User: ${target.username}`,
+						title: `User: ${target.user.username}`,
 						description: `This user does not have any vouches!`,
 						color: 'RANDOM',
 					},
 				});
 			}
-			let rating = 'potential scammer';
-			if (data.Vouches > 2) {
-				rating = 'pro';
+			const role = '863489844557905971';
+			let rating = 'Needs MM/Sus Ngl';
+			let emoji = '🥉';
+			if (data.Vouches > 25) {
+				rating = 'Regular trader';
+				emoji = '🥈';
+			}
+			if (data.Vouches >= 76) {
+				rating = 'Elite Trader';
+				emoji = '🥇';
+				target.roles.add(role);
 			}
 
 			message.channel.send(
 				new Discord.MessageEmbed()
 					.setColor('RANDOM')
 					.setAuthor(
-						`${target.username}'s vouches`,
-						target.avatarURL({ dynamic: true })
+						`${target.user.username}'s vouches`,
+						target.user.displayAvatarURL({ dynamic: true })
 					)
 					.addFields({
 						name: `total`,
 						value: `\`${data.Vouches}\``,
 						inline: true,
 					})
-					.setFooter(`trust rating: ${rating}`)
+					.setFooter(`${emoji} trust rating: ${rating}`)
 			);
 		}
 	},
