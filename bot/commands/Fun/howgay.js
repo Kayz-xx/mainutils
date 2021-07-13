e072bef9093994a486d1946031df2b9a44cd419f
Kayz-xx <83003158+Kayz-xx@users.noreply.github.com>
2021-07-13 12:50:09 +0530

    Create howgay.js

---
+++ bot/commands/Fun/howgay.js
@@ -0,0 +1,27 @@
+const { MessageButton, MessageActionRow } = require('discord-buttons')
+const Discord = require('discord.js')
+
+
+
+module.exports = {
+    name: 'howgay',
+    aliases: ['gaypercent'],
+    cooldown: '0',
+    permissions: [],
+    category: 'Misc',
+    description: 'Just a regular howgay command',
+
+  
+    async execute(client, message, cmd,  args) {
+        let member = args[0] || message.author
+
+        let random = Math.floor(Math.random() * 100)
+
+        const embed = new Discord.MessageEmbed()
+        .setTitle('Gay Rate Machine')
+        .setDescription(`${member} are ${random}% Gay🌈`)
+        .setColor("BLUE")
+        message.channel.send(embed)
+    }
+
+}
