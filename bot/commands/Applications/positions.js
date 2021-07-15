73e2521bf3120aae638045fa68cac6fa2bba2ad7
Kayz-xx <83003158+Kayz-xx@users.noreply.github.com>
2021-07-15 11:25:44 +0530

    Update positions.js

--- bot/commands/Applications/positions.js
+++ bot/commands/Applications/positions.js
@@ -20,14 +20,18 @@ module.exports = {
     
     db.ref(`Applications/${message.guild.id}`)
 
+    String.prototype.capitalize = function() {
+      return this.charAt(0).toUpperCase() + this.slice(1);
+  }
+
 
     let exampleEmbed = new Discord.MessageEmbed()
 	.setColor('#0099ff')
 	.setTitle('Positions Availible')
 	.addFields(
-		{ name: data.Positions0.Name, value: `Total Questions: 7 \n Status: ${data.Positions0.Status}`, inline: true },
-		{ name: data.Positions1.Name, value: `Total Questions: 7 \n Status: ${data.Positions1.Status}`, inline: true },
-    { name: data.Positions2.Name, value: `Total Questions: 10 \n Status: ${data.Positions2.Status}`, inline: true },
+		{ name: data.Positions0.Name, value: `Total Questions: 7 \n Status: ${data.Positions0.Status.capitalize()}`, inline: true },
+		{ name: data.Positions1.Name, value: `Total Questions: 7 \n Status: ${data.Positions1.Status.capitalize()}`, inline: true },
+    { name: data.Positions2.Name, value: `Total Questions: 11 \n Status: ${data.Positions2.Status.capitalize()}`, inline: true },
 	) 
 message.channel.send(exampleEmbed)
     }
