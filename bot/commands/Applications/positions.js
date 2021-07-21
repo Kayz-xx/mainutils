cd2c36f78803619da44fd808eae630711487cbb6
Kayz-xx <83003158+Kayz-xx@users.noreply.github.com>
2021-07-21 20:05:20 +0530

    Update positions.js

--- bot/commands/Applications/positions.js
+++ bot/commands/Applications/positions.js
@@ -29,8 +29,8 @@ module.exports = {
 	.setColor('#7C83FD')
 	.setTitle('Positions Available')
 	.addFields(
-		{ name: data.Positions0.Name, value: `Total Questions: 6 \n Status: ${data.Positions0.Status.capitalize()}`, inline: true },
-		{ name: data.Positions1.Name, value: `Total Questions: 7 \n Status: ${data.Positions1.Status.capitalize()}`, inline: true },
+		{ name: data.Positions0.Name, value: `Total Questions: 7 \n Status: ${data.Positions0.Status.capitalize()}`, inline: true },
+		{ name: data.Positions1.Name, value: `Total Questions: 6 \n Status: ${data.Positions1.Status.capitalize()}`, inline: true },
     		{ name: data.Positions2.Name, value: `Total Questions: 12 \n Status: ${data.Positions2.Status.capitalize()}`, inline: true },
 	) 
 message.channel.send(exampleEmbed)
