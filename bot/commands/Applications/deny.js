bdac13ec9c3a11740e2239011e788c2e12391c4f
Kayz-xx <83003158+Kayz-xx@users.noreply.github.com>
2021-07-25 11:02:09 +0530

    Update deny.js

--- bot/commands/Applications/deny.js
+++ bot/commands/Applications/deny.js
@@ -1,43 +1,51 @@
-const Discord = require('discord.js')
-const {db} = require('../../firebase')
-const { MessageEmbed } = require ('discord.js')
+const Discord = require('discord.js');
+const { db } = require('../../firebase');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = {
-  name: 'deny',
-  aliases: ['deny'],
-  cooldown: '0',
-  category: 'Applications',
-  permissions: [],
-  description: "This a command that denies applicants. (Only Admins can use this command)",
-  
-  async execute(client, message, cmd,  args) {
-    if(!message.member.permissions.has("ADMINISTRATOR")) return message.channel.send("Only admins can reject applicants!")
-    message.react("<a:EE_thumbsup:848244792802541628>")
-    const messageID = args[0]
-    const denyQuery = args.slice(1).join(" ")
-    try{
-        const applicationChannel = message.guild.channels.cache.get('764885370558349373')
-        const applicationdEmbed = await applicationChannel.messages.fetch(messageID)
-        
-        const data = applicationdEmbed.embeds[0]
-        const acceptEmbed = new MessageEmbed()
-        .setTitle(data.title)
-        .setAuthor(data.author.name)
-        .setDescription(data.description)
-        .setColor(data.color)
-        .addField("Status **(Denied)**", denyQuery)
+	name: 'deny',
+	aliases: ['deny'],
+	cooldown: '0',
+	category: 'Applications',
+	permissions: [],
+	description:
+		'This a command that denies applicants. (Only Admins can use this command)',
 
-        
-			applicationdEmbed.edit({ embed: acceptEmbed});
+	async execute(client, message, cmd, args) {
+		if (!message.member.permissions.has('ADMINISTRATOR')) return message.channel.send('This command can only be used by administrators!')
+		const messageID = args[1];
+		const channelID = args[0]
+		let denyQuery = args.slice(2).join(' ').toLowerCase()
+		try{
+			const applicationChannel =
+				message.guild.channels.cache.get(channelID)
+			const applicationdEmbed = await applicationChannel.messages.fetch(
+				messageID
+			);
 
-        const user = await client.users.cache.find((u) => u.tag === data.author.name)
-        user.send({  embed : {
-            description : 'Your application has been denied, thank you for applying!',
-            color : "#77ACF1"
-        }})
-       // user.roles.add(data3.Staffrole)
-    } catch (err) {
-        console.log(err)
-    }
-  }
-}
+			const data = applicationdEmbed.embeds[0];
+			const acceptEmbed = new MessageEmbed()
+				.setTitle(data.title)
+				.setAuthor(data.author.name)
+				.setDescription(data.description)
+				.setColor(data.color)
+				.addField('Status **(Denied)**', denyQuery);
+
+			applicationdEmbed.edit({ embed: acceptEmbed });
+
+			const user = await client.users.cache.find(
+				(u) => u.tag === data.author.name
+			);
+			user.send({
+				embed: {
+					description:
+						'Your application has been denied, thank you for applying!',
+					color: '#77ACF1',
+				},
+			});
+			// user.roles.add(data3.Staffrole)
+		} catch (err) {
+			console.log(err);
+		}
+	},
+};
