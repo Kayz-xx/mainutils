deb6ed7f2165cba1f409a6b91f0bd51532f84311
Kayz-xx <83003158+Kayz-xx@users.noreply.github.com>
2021-07-29 13:53:09 +0530

    Update leaderboard.js

--- bot/commands/Donations/leaderboard.js
+++ bot/commands/Donations/leaderboard.js
@@ -1,4 +1,4 @@
-const {db} = require('../../firebase')
+
 const { Client, Message, MessageEmbed, Collection } = require('discord.js')
 const economy = require('../../economy')
 const formatter = new Intl.NumberFormat('en')
@@ -7,7 +7,7 @@ const formatter = new Intl.NumberFormat('en')
 
 module.exports = {
     name: 'leaderboard',
-    aliases: ['leaderboard'],
+    aliases: ['lb'],
     cooldown: '0',
     usage: '',
     permissions: [],
@@ -16,37 +16,22 @@ module.exports = {
     description: 'Donation Leaderboard',
     
     async execute(client, message, cmd,  args) {
+            const guildId = message.guild.id
+        const data = await economy.getDonation(guildId);
+        let desc = ""
+            
+        for (let i = 0; i < Math.min(10, data.length); i ++) {
+          let user = data [i];
+          desc += `${i}) <@!${user.userId}> - **${user.coins} coins**\n`
+        };
 
-            const collection = new Collection();
+        const embed = new MessageEmbed ()
+        .setTitle(`Donation Leaderboard in ${message.guild.name}`)
+        .setDescription(desc)
+        .setFooter(`These are normal donations`)
+        .setColor('88FFF7')
 
-            await Promise.all(
-                message.guild.members.cache.map(async(member) => {
-                     const guildId = message.guild.id
-                     const userId = member.id
-                    const donation = await economy.getDonation(guildId, userId);
-                return donation !== 0 ? collection.set(userId, {
-                    userId,
-                    donation,
-                })
-                : null
-                })
-            )
-            const data = collection.sort((a, b) => b.donation - a.donation).first(10);
+        message.channel.send(embed)
 
- 
-            db.ref(`Donations/Info/${message.guild.name}/Leaderboard`).set(data)
-
-            message.channel.send(
-                new MessageEmbed()
-                .setTitle(`Donation Leaderboard in ${message.guild.name}`)
-                .setDescription(
-                    data.map((v, i) => {
-                    return `${i+1}) ${client.users.cache.get(v.userId).tag} - **${formatter.format(v.donation)} coins**`
-                }))
-                .setFooter(`These are normal donations`)
-                 )
-                }
-
-            }
-        
-    
+    }
+}
