3e5c6825e780902c7e68dd772b0f1307f0c8a648
Kayz-xx <83003158+Kayz-xx@users.noreply.github.com>
2021-07-29 14:05:06 +0530

    Update eventlb.js

--- bot/commands/Donations/eventlb.js
+++ bot/commands/Donations/eventlb.js
@@ -1,6 +1,5 @@
 const {db} = require('../../firebase')
 const { Client, Message, MessageEmbed, Collection } = require('discord.js')
-const economy = require('../../economy')
 const formatter = new Intl.NumberFormat('en')
 const eventdonations = require('../../eventdonations')
 
@@ -16,32 +15,23 @@ module.exports = {
     
     async execute(client, message, cmd,  args) {
 
-            const collection = new Collection();
+     
+            const guildId = message.guild.id
+        const data = await eventdonations.getDonation(guildId);
+        let desc = ""
 
-            await Promise.all(
-                message.guild.members.cache.map(async(member) => {
-                     const guildId = message.guild.id
-                     const userId = member.id
-                    const donation = await eventdonations.getDonation(guildId, userId)
-                return donation !== 0 ? collection.set(userId, {
-                    userId,
-                    donation,
-                })
-                : null
-                })
-            )
-            const data = collection.sort((a, b) => b.donation - a.donation).first(10); 
+        for (let i = 0; i < Math.min(10, data.length); i ++) {
+          let user = data [i];
+          desc += `${i+1}) <@!${user.userId}> - **${formatter.format(user.eventcoins)} coins**\n`
+        };
 
-            db.ref(`Donations/Info/Event Leaderboard`).set(data)
+        const embed = new MessageEmbed ()
+        .setTitle(`Event Donation Leaderboard in ${message.guild.name}`)
+        .setDescription(desc)
+        .setFooter(`These are event donations`)
+        .setColor('88FFF7')
 
-            message.channel.send(
-                new MessageEmbed()
-                .setTitle(`Event Donation Leaderboard in ${message.guild.name}`)
-                .setDescription(
-                    data.map((v, i) => {
-                    return `${i+1}) ${client.users.cache.get(v.userId).tag} - **${formatter.format(v.donation)} coins**`
-                }))
-                .setFooter(`These are event donations`)
-                 )
-            }
+        message.channel.send(embed)
+
     }
+}
