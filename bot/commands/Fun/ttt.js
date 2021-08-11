7f79231e401864d501ca60bf609a78bbedf09740
Kayz-xx <83003158+Kayz-xx@users.noreply.github.com>
2021-08-11 09:24:38 +0530

    Update ttt.js

--- bot/commands/Fun/ttt.js
+++ bot/commands/Fun/ttt.js
@@ -9,7 +9,9 @@ module.exports = {
   async execute(client, message, cmd,  args) {
 
     const opponent = message.mentions.users.first();
+
     if (!opponent) return message.channel.send(`Please mention who you want to challenge at tictactoe.`);
+    if(opponent.bot) return message.channel.send(`You can not play with bots.`);
     const { TicTacToe } = require('weky')
     const game = new TicTacToe({
         message: message,
