81d7a2ef5ba90da44ace95dbd935f32c71f22575
Kayz-xx <83003158+Kayz-xx@users.noreply.github.com>
2021-08-26 11:39:08 +0530

    Update and rename presenceUpdate to presenceUpdate.js

---
+++ bot/events/presenceUpdate.js
@@ -0,0 +1,29 @@
+
+const { MessageEmbed } = require("discord.js");
+
+module.exports.run = (client, oldPresence, newPresence) => {
+    const role = newPresence.guild.roles.cache.get("872900600046690346");
+    const member = newPresence.member
+    const guild = newPresence.guild
+    if(newPresence.activities) {
+    newPresence.activities.forEach((activity) => {
+        if(activity.name == "Custom Status") {
+            if(activity.state == null) return;
+            let statuses = ['.gg/elites', 'discord.gg/elites'];
+            if (statuses.some(x => activity.state.includes(x))) {
+              let channel = guild.channels.cache.find(x => x.id === "764885369530744868")
+              channel.send(new MessageEmbed()
+            .setAuthor(member.displayName)
+            .setDescription(`<@&872900600046690346> added to ${member.displayName}`)
+            .setFooter(member.id)
+            )
+              return member.roles.add(role)
+            } else {
+              if(member.roles.cache.get(role.id)) {
+               member.roles.remove(role)
+              }
+            }
+        }
+    })
+    }
+}
