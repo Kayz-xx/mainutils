fea649afa37e2cca256f8e2951bca53b9fd9df7d
Kayz-xx <83003158+Kayz-xx@users.noreply.github.com>
2021-09-01 16:58:17 +0530

    Update giveaway.js

--- commands/Misc/giveaway.js
+++ commands/Misc/giveaway.js
@@ -12,14 +12,14 @@ module.exports = {
       if(message.member.roles.cache.some(x => x.id === '768129052623372348')) return; 
     setTimeout(() => message.delete(), 100)
       const msg = args.slice(0).join(' ');
-      message.channel.send({content:`<@&859323544138809364> ${msg} **- ${message.author.tag}**`})
+      message.channel.send({content:`<@&768129052623372348> ${msg} **- ${message.author.tag}**`})
         }
      } if(cmd === 'eventping'){
         if(message.guild.id === "764885367160700958") {
         if(message.member.roles.cache.some(x => x.id === '792799102140022785')) return; 
         setTimeout(() => message.delete(), 100)
         const msg = args.slice(0).join(' ');
-        message.channel.send({content:`<@&859323607678844928> ${msg} **- ${message.author.tag}**`})
+        message.channel.send({content:`<@&792799102140022785> ${msg} **- ${message.author.tag}**`})
         }
      }
   }
