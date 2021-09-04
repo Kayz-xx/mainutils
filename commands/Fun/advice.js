889beda25a5d44fd6af0f2f843e936a82a86b210
Kayz-xx <83003158+Kayz-xx@users.noreply.github.com>
2021-09-04 22:11:25 +0530

    Update advice.js

--- commands/Fun/advice.js
+++ commands/Fun/advice.js
@@ -1,18 +1,12 @@
 const { Random } = require("something-random-on-discord")
- 
-module.exports = {
-  name: "advice",
-  aliases: ['aid'],
-   category: "fun",
-   
-  
-  description: "Get Fresh Advice :D",
-  async execute(client, message, cmd,  args) {
 
-    if(cmd === 'advice'){
-  
-    let data = await Random.getAdvice()
-    message.channel.send({content: data})
-    }
-}
+module.exports = {
+  name: "meme",
+  category: "fun",
+
+  description: "Get Fresh meme :D",
+  async execute(client, message, cmd,  args) {
+    let data = await Random.getMeme()
+    message.channel.send({embeds: [data.embed]})
+  }
 }
