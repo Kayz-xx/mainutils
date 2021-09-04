625ba1e651dc6de78c4e768a2f03e268eccf38cc
Kayz-xx <83003158+Kayz-xx@users.noreply.github.com>
2021-09-05 02:15:18 +0530

    Update heist.js

--- commands/Misc/heist.js
+++ commands/Misc/heist.js
@@ -5,7 +5,7 @@ module.exports = {
     aliases: [],
     cooldown: '0',
     permissions: [],
-    description: 'Heist Ping!',
+    description: 'Heist ping!',
     category: 'Misc',
 
     async execute(client, message, cmd,  args) {
