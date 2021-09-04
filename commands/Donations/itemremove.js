6d14d24c7757853913764aec65af89e882b6555c
Kayz-xx <83003158+Kayz-xx@users.noreply.github.com>
2021-09-05 02:02:54 +0530

    Update itemremove.js

--- commands/Donations/itemremove.js
+++ commands/Donations/itemremove.js
@@ -12,6 +12,7 @@ module.exports = {
     category: 'Donations',
     
     async execute(client, message, cmd,  args) {
+         if(!message.member.roles.cache.has("764885367400693764")) return;
         let data = await db
         .ref(`Donations/Info/${message.guild.id}/List`)
         .once("value")
