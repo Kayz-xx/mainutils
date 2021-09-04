e0594eee933b0fe9003eebcab18bf1810acef135
Kayz-xx <83003158+Kayz-xx@users.noreply.github.com>
2021-09-05 02:20:43 +0530

    Update giveaway.js

--- commands/Misc/giveaway.js
+++ commands/Misc/giveaway.js
@@ -8,14 +8,14 @@ module.exports = {
     async execute(client, message, cmd,  args) {
     if(cmd === 'giveaway'){
         if(message.guild.id === "764885367160700958") {
-      if(!message.member.roles.cache.some(x => x.id === '768129052623372348') || !message.member.permissions.has(Permissions.FLAGS.MANAGE_MESSAGES)) return; 
+      if(!message.member.roles.cache.some(x => x.id === '768129052623372348') && !message.member.permissions.has(Permissions.FLAGS.MANAGE_MESSAGES)) return
     setTimeout(() => message.delete(), 100)
       const msg = args.slice(0).join(' ');
       message.channel.send({content:`<@&764885367241048064> ${msg} **- ${message.author.tag}**`})
         }
      } if(cmd === 'eping'){
         if(message.guild.id === "764885367160700958") {
-      if(!message.member.roles.cache.some(x => x.id === '768129052623372348') || !message.member.permissions.has(Permissions.FLAGS.MANAGE_MESSAGES)) return; 
+          if(!message.member.roles.cache.some(x => x.id === '768129052623372348') && !message.member.permissions.has(Permissions.FLAGS.MANAGE_MESSAGES)) return 
         setTimeout(() => message.delete(), 100)
         const msg = args.slice(0).join(' ');
         message.channel.send({content:`<@&778355810043559976> ${msg} **- ${message.author.tag}**`})
@@ -23,7 +23,7 @@ module.exports = {
      }
      if(cmd === 'fping'){
       if(message.guild.id === "764885367160700958") {
-      if(!message.member.roles.cache.some(x => x.id === '768129052623372348') || !message.member.permissions.has(Permissions.FLAGS.MANAGE_MESSAGES)) return; 
+        if(!message.member.roles.cache.some(x => x.id === '768129052623372348') && !message.member.permissions.has(Permissions.FLAGS.MANAGE_MESSAGES)) return 
       setTimeout(() => message.delete(), 100)
       const msg = args.slice(0).join(' ');
       message.channel.send({content:`<@&793676669550329897> ${msg} **- ${message.author.tag}**`})
@@ -31,7 +31,7 @@ module.exports = {
     }
     if(cmd === 'mping'){
       if(message.guild.id === "764885367160700958") {
-        if(!message.member.roles.cache.some(x => x.id === '768129052623372348') || !message.member.permissions.has(Permissions.FLAGS.MANAGE_MESSAGES)) return; 
+        if(!message.member.roles.cache.some(x => x.id === '768129052623372348') && !message.member.permissions.has(Permissions.FLAGS.MANAGE_MESSAGES)) return 
       setTimeout(() => message.delete(), 100)
       const msg = args.slice(0).join(' ');
       message.channel.send({content:`<@&794315002449231893> ${msg} **- ${message.author.tag}**`})
