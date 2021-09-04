530113d9c62d85413ebbdfa016e32d2e7be541dd
Kayz-xx <83003158+Kayz-xx@users.noreply.github.com>
2021-09-05 01:02:24 +0530

    Create heist.js

---
+++ commands/Misc/heist.js
@@ -0,0 +1,36 @@
+const {MessageEmbed, Permissions} = require('discord.js')
+
+module.exports = {
+    name: 'heist',
+    aliases: [],
+    cooldown: '0',
+    permissions: [],
+    description: 'Heist Ping!',
+    category: 'Misc',
+
+    async execute(client, message, cmd,  args) {
+      if(!message.member.roles.cache.has("770365283147317248") && message.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) return;
+      const prize = args[0]
+      if(!prize) return message.channel.send("Please specify an amount")
+      const time = args[1]
+      if(!time) return message.channel.send("Please specify an amount of time")
+      const requirement = args[2]
+      if(!requirement) return message.channel.send("Please specify a requirement")
+      const donor = client.users.cache.get(args[3]) || message.mentions.members.first()
+      if(!donor) return message.channel.send("Please specify a donor")
+      const msge = args.slice(4).join(' ');
+      if(!msge) return message.channel.send("Please specify a message")
+  
+      const embed = new MessageEmbed()
+      .setAuthor('Heist!', message.author.avatarURL({ dynamic:true }))
+      .setTitle('Heist Time')
+      .addField(`<a:im5:859288337280925746> Time`, `${time}`) 
+      .addField(`<a:im5:859288337280925746> Donor`, `${donor}`) 
+      .addField(`<a:im5:859288337280925746> Requirement`, requirement) 
+      .addField(`<a:im5:859288337280925746> Message`, msge) 
+      .setThumbnail('https://cdn.discordapp.com/icons/764885367160700958/a_38503e9dec18ac442fecaad24a3d07c0.gif?size=1024')
+      .setColor('RANDOM')
+      .setImage(`https://cdn.discordapp.com/attachments/855828767846039582/859816740987600906/tnbIcOg.png`)
+      message.channel.send({content: `<@&767787322133184542>`, embeds: [embed]})
+    }
+  }
