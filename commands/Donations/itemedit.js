26955ba033196a4909a09681c91969c9fc3bf83d
Kayz-xx <83003158+Kayz-xx@users.noreply.github.com>
2021-09-04 23:28:28 +0530

    Create itemedit.js

---
+++ commands/Donations/itemedit.js
@@ -0,0 +1,44 @@
+const { Client, Message, MessageEmbed, Collection } = require('discord.js')
+const formatter = new Intl.NumberFormat('en')
+const {db} = require('../../firebase.js')
+
+
+module.exports = {
+    name: 'itemedit',
+    aliases: [],
+    cooldown: '0',
+    usage: '<name> <amount>',
+    permissions: [],
+    category: 'Donations',
+    
+    async execute(client, message, cmd,  args) {
+        let name = args[0]
+        if(!name)
+        return message.channel.send({content: "Specify an item to edit."})
+        let amount = args[1]
+        if(!amount)
+        return message.channel.send({content: "Specify an amount"})
+        let data = await db
+        .ref(`Donations/Info/${message.guild.id}/List`)
+        .once("value")
+        .then(snapshot => snapshot.val())|| []
+        const found = data.find(item => item.name === name) || data.find(item => item.aliases === name)
+        const place = data.indexOf(found)
+        if(found) {
+            data[place] = {
+                "name" : found.name,
+                "amount" : parseInt(amount),
+                "aliases": found.aliases,
+                "type" : found.type
+              }
+            db.ref(`Donations/Info/${message.guild.id}/List`).set(data)
+            const item = data.find(item => item.name === name) || data.find(item => item.aliases === name)
+            let embed6 = new MessageEmbed()
+            .setAuthor(`Item ${name} price set to: ${amount}`)
+            .setColor("RANDOM")
+            message.channel.send({embeds: [embed6]})
+        } else {
+            return message.reply({content: 'Could not find that item.'})
+        }
+    }
+}
