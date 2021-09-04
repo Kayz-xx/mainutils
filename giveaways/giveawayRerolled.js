24fbfc4939a35ad4f401108c97fa7d85e7cc1126
Kayz-xx <83003158+Kayz-xx@users.noreply.github.com>
2021-09-05 01:56:53 +0530

    Update giveawayRerolled.js

--- giveaways/giveawayRerolled.js
+++ giveaways/giveawayRerolled.js
@@ -7,8 +7,8 @@ module.exports.run = async (client, giveaway, winners) => {
     const guild = client.guilds.cache.get(giveaway.guildId)
     winners.forEach((member) => {
         let embed = new MessageEmbed()
-        .setTitle("Your won a giveaway!")
-        .setDescription(`Congratulations! You just won a giveaway in ${guild.name}!\n\n Please wait patiently to receive your payout. If you DM the host or contacting support before 12 hours, you will get rerolled and receive the \`No Giveaways\` role.`)
+        .setTitle("You won a giveaway!")
+        .setDescription(`Congratulations! You just won a giveaway in ${guild.name}<a:nya6:883810109539635272>!\n\n Please wait patiently to receive your payout. If you DM the host or contacting support before 12 hours, you will get rerolled and receive the \`No Giveaways\` role.`)
         .setColor("B5FFD9")
         .addField('Giveaway', `[${giveaway.prize}](${giveaway.messageURL})`)
         .setFooter(guild.name, guild.iconURL())
