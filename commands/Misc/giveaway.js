2d2a5271cbe6846a1465ac83f505878762de88d5
Kayz-xx <83003158+Kayz-xx@users.noreply.github.com>
2021-09-05 11:54:52 +0530

    Update giveaway.js

--- commands/Misc/giveaway.js
+++ commands/Misc/giveaway.js
@@ -1,14 +1,14 @@
 const {Permissions, MessageEmbed} = require("discord.js")
 module.exports = {
     name: 'giveaway',
-    aliases: ['eping', 'fping', 'mping'],
+    aliases: ['eping', 'fping', 'mping', 'middleman'],
     cooldown: 0,
     category: 'Misc',
 
     async execute(client, message, cmd,  args) {
     if(cmd === 'giveaway'){
         if(message.guild.id === "764885367160700958") {
-      if(!message.member.roles.cache.some(x => x.id === '768129052623372348') && !message.member.permissions.has(Permissions.FLAGS.MANAGE_MESSAGES)) return 
+      if(!message.member.roles.cache.some(x => x.id === '768129052623372348') || !message.member.permissions.has(Permissions.FLAGS.MANAGE_MESSAGES)) return 
     setTimeout(() => message.delete(), 100)
       const msg = args.slice(0).join(' ');
       message.channel.send({content:`<@&764885367241048064> ${msg} **- ${message.author.tag}**`})
@@ -33,6 +33,14 @@ module.exports = {
     if(cmd === 'mping'){
       if(message.guild.id === "764885367160700958") {
         if(!message.member.roles.cache.some(x => x.id === '792799102140022785') && !message.member.permissions.has(Permissions.FLAGS.MANAGE_MESSAGES)) return 
+      setTimeout(() => message.delete(), 100)
+      const msg = args.slice(0).join(' ');
+      message.channel.send({content:`<@&794315002449231893> ${msg} **- ${message.author.tag}**`})
+      }
+    }
+    if(cmd === 'middleman'){
+      if(message.guild.id === "764885367160700958") {
+      if(message.channel.id !== "805595301950783538") return;
       setTimeout(() => message.delete(), 100)
       const msg = args.slice(0).join(' ');
       message.channel.send({content:`<@&794315002449231893> ${msg} **- ${message.author.tag}**`})
