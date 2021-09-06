881cf2f831dcbe2cf182b494dae836011b87b101
Kayz-xx <83003158+Kayz-xx@users.noreply.github.com>
2021-09-06 21:00:46 +0530

    Update ttt.js

--- commands/Fun/ttt.js
+++ commands/Fun/ttt.js
@@ -68,62 +68,12 @@ try {
             emoji: '<:blank:873573058709950514>'
         }
     }
+    console.log(Args)
+    console.log(fighters[0])
     let { MessageButton, MessageActionRow } = require('discord.js')
     let msg = await message.channel.send({content: `**${message.member.displayName} vs ${opponent.displayName}**\n **TicTacToe** | <@!${Args.userid}>'s turn (⭕)`})
     tictactoe(msg)
     async function tictactoe(m) {
-      let a1 = new MessageButton()
-      .setStyle(Args.a1.style)
-      .setCustomId('a1')
-      .setEmoji(Args.a1.emoji)
-      .setDisabled(Args.a1.disabled);
-  let a2 = new MessageButton()
-      .setStyle(Args.a2.style)
-      .setEmoji(Args.a2.emoji)
-      .setCustomId('a2')
-      .setDisabled(Args.a2.disabled);
-  let a3 = new MessageButton()
-      .setStyle(Args.a3.style)
-      .setEmoji(Args.a3.emoji)
-      .setCustomId('a3')
-      .setDisabled(Args.a3.disabled);
-  let b1 = new MessageButton()
-      .setStyle(Args.b1.style)
-      .setEmoji(Args.b1.emoji)
-      .setCustomId('b1')
-      .setDisabled(Args.b1.disabled);
-  let b2 = new MessageButton()
-      .setStyle(Args.b2.style)
-      .setEmoji(Args.b2.emoji)
-      .setCustomId('b2')
-      .setDisabled(Args.b2.disabled);
-  let b3 = new MessageButton()
-      .setStyle(Args.b3.style)
-      .setEmoji(Args.b3.emoji)
-      .setCustomId('b3')
-      .setDisabled(Args.b3.disabled);
-  let c1 = new MessageButton()
-      .setStyle(Args.c1.style)
-      .setEmoji(Args.c1.emoji)
-      .setCustomId('c1')
-      .setDisabled(Args.c1.disabled);
-  let c2 = new MessageButton()
-      .setStyle(Args.c2.style)
-      .setEmoji(Args.c2.emoji)
-      .setCustomId('c2')
-      .setDisabled(Args.c2.disabled);
-  let c3 = new MessageButton()
-      .setStyle(Args.c3.style)
-      .setEmoji(Args.c3.emoji)
-      .setCustomId('c3')
-      .setDisabled(Args.c3.disabled);
-  let a = new MessageActionRow()
-      .addComponents(a1, a2, a3)
-  let b = new MessageActionRow()
-      .addComponents(b1, b2, b3)
-  let c = new MessageActionRow()
-      .addComponents(c1, c2, c3)
-      m.edit({content: `**${message.member.displayName} vs ${opponent.displayName}**\n**TicTacToe** | <@!${Args.userid}>'s turn (${Args.user == 0 ? "⭕" : "❌"})`, components: [a, b, c]})
         Args.userid=fighters[Args.user]
         let won = {
             "⭕": false,
@@ -138,16 +88,68 @@ try {
         if (Args.b1.emoji == "<:TTTO:873550866915942430>" && Args.b2.emoji == "<:TTTO:873550866915942430>" && Args.b3.emoji == "<:TTTO:873550866915942430>") won["⭕"] = true
         if (Args.c1.emoji == "<:TTTO:873550866915942430>" && Args.c2.emoji == "<:TTTO:873550866915942430>" && Args.c3.emoji == "<:TTTO:873550866915942430>") won["⭕"] = true
         if (won["⭕"] != false) return m.edit({content:'⭕ won!', components: []})
-        if (Args.a1.emoji == "<:TTTX:873550878727098428>" && Args.b1.emoji == "<:TTTX:873550878727098428>" && Args.c1.emoji == "<:TTTX:873550878727098428>") won["❌"] = true
-        if (Args.a2.emoji == "<:TTTX:873550878727098428>" && Args.b2.emoji == "<:TTTX:873550878727098428>" && Args.c2.emoji == "<:TTTX:873550878727098428>") won["❌"] = true
-        if (Args.a3.emoji == "<:TTTX:873550878727098428>" && Args.b3.emoji == "<:TTTX:873550878727098428>" && Args.c3.emoji == "<:TTTX:873550878727098428>") won["❌"] = true
-        if (Args.a1.emoji == "<:TTTX:873550878727098428>" && Args.b2.emoji == "<:TTTX:873550878727098428>" && Args.c3.emoji == "<:TTTX:873550878727098428>") won["❌"] = true
-        if (Args.a3.emoji == "<:TTTX:873550878727098428>" && Args.b2.emoji == "<:TTTX:873550878727098428>" && Args.c1.emoji == "<:TTTX:873550878727098428>") won["❌"] = true
-        if (Args.a1.emoji == "<:TTTX:873550878727098428>" && Args.a2.emoji == "<:TTTX:873550878727098428>" && Args.a3.emoji == "<:TTTX:873550878727098428>") won["❌"] = true
-        if (Args.b1.emoji == "<:TTTX:873550878727098428>" && Args.b2.emoji == "<:TTTX:873550878727098428>" && Args.b3.emoji == "<:TTTX:873550878727098428>") won["❌"] = true
-        if (Args.c1.emoji == "<:TTTX:873550878727098428>" && Args.c2.emoji == "<:TTTX:873550878727098428>" && Args.c3.emoji == "<:TTTX:873550878727098428>") won["❌"] = true
+        if (Args.a1.emoji == "<:TTTX:875313137019142146>" && Args.b1.emoji == "<:TTTX:875313137019142146>" && Args.c1.emoji == "<:TTTX:875313137019142146>") won["❌"] = true
+        if (Args.a2.emoji == "<:TTTX:875313137019142146>" && Args.b2.emoji == "<:TTTX:875313137019142146>" && Args.c2.emoji == "<:TTTX:875313137019142146>") won["❌"] = true
+        if (Args.a3.emoji == "<:TTTX:875313137019142146>" && Args.b3.emoji == "<:TTTX:875313137019142146>" && Args.c3.emoji == "<:TTTX:875313137019142146>") won["❌"] = true
+        if (Args.a1.emoji == "<:TTTX:875313137019142146>" && Args.b2.emoji == "<:TTTX:875313137019142146>" && Args.c3.emoji == "<:TTTX:875313137019142146>") won["❌"] = true
+        if (Args.a3.emoji == "<:TTTX:875313137019142146>" && Args.b2.emoji == "<:TTTX:875313137019142146>" && Args.c1.emoji == "<:TTTX:875313137019142146>") won["❌"] = true
+        if (Args.a1.emoji == "<:TTTX:875313137019142146>" && Args.a2.emoji == "<:TTTX:875313137019142146>" && Args.a3.emoji == "<:TTTX:875313137019142146>") won["❌"] = true
+        if (Args.b1.emoji == "<:TTTX:875313137019142146>" && Args.b2.emoji == "<:TTTX:875313137019142146>" && Args.b3.emoji == "<:TTTX:875313137019142146>") won["❌"] = true
+        if (Args.c1.emoji == "<:TTTX:875313137019142146>" && Args.c2.emoji == "<:TTTX:875313137019142146>" && Args.c3.emoji == "<:TTTX:875313137019142146>") won["❌"] = true
         if (won["❌"] != false) return m.edit({content:'❌ won!', components: []})
-    
+
+        let a1 = new MessageButton()
+        .setStyle(Args.a1.style)
+        .setCustomId('a1')
+        .setEmoji(Args.a1.emoji)
+        .setDisabled(Args.a1.disabled);
+    let a2 = new MessageButton()
+        .setStyle(Args.a2.style)
+        .setEmoji(Args.a2.emoji)
+        .setCustomId('a2')
+        .setDisabled(Args.a2.disabled);
+    let a3 = new MessageButton()
+        .setStyle(Args.a3.style)
+        .setEmoji(Args.a3.emoji)
+        .setCustomId('a3')
+        .setDisabled(Args.a3.disabled);
+    let b1 = new MessageButton()
+        .setStyle(Args.b1.style)
+        .setEmoji(Args.b1.emoji)
+        .setCustomId('b1')
+        .setDisabled(Args.b1.disabled);
+    let b2 = new MessageButton()
+        .setStyle(Args.b2.style)
+        .setEmoji(Args.b2.emoji)
+        .setCustomId('b2')
+        .setDisabled(Args.b2.disabled);
+    let b3 = new MessageButton()
+        .setStyle(Args.b3.style)
+        .setEmoji(Args.b3.emoji)
+        .setCustomId('b3')
+        .setDisabled(Args.b3.disabled);
+    let c1 = new MessageButton()
+        .setStyle(Args.c1.style)
+        .setEmoji(Args.c1.emoji)
+        .setCustomId('c1')
+        .setDisabled(Args.c1.disabled);
+    let c2 = new MessageButton()
+        .setStyle(Args.c2.style)
+        .setEmoji(Args.c2.emoji)
+        .setCustomId('c2')
+        .setDisabled(Args.c2.disabled);
+    let c3 = new MessageButton()
+        .setStyle(Args.c3.style)
+        .setEmoji(Args.c3.emoji)
+        .setCustomId('c3')
+        .setDisabled(Args.c3.disabled);
+    let a = new MessageActionRow()
+        .addComponents(a1, a2, a3)
+    let b = new MessageActionRow()
+        .addComponents(b1, b2, b3)
+    let c = new MessageActionRow()
+        .addComponents(c1, c2, c3)
+    m.edit({content: `**${message.member.displayName} vs ${opponent.displayName}**\n**TicTacToe** | <@!${Args.userid}>'s turn (${Args.user == 0 ? "⭕" : "❌"})`, components: [a, b, c]})
         const filter = (button) => button.user.id === Args.userid;
         const collector = msg.createMessageComponentCollector({
           filter, max: 1, 
@@ -167,7 +169,7 @@ try {
                 Args.user = 0
                 Args[b.customId] = {
                   style: "PRIMARY",
-                  emoji: '<:TTTX:873550878727098428>',
+                  emoji: '<:TTTX:875313137019142146>',
                   disabled: true
               }
           
