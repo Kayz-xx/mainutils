1c8c09b8745a4ec8046b11677714b05c0bf77126
Kayz-xx <83003158+Kayz-xx@users.noreply.github.com>
2021-09-07 14:18:59 +0530

    Update info.js

--- commands/Misc/info.js
+++ commands/Misc/info.js
@@ -15,20 +15,20 @@ module.exports = {
       const roles = message.guild.roles.cache.sort((a, b) => b.position - a.position).map(role => role.toString());
       let txt = '<:txtchannel:872428761478463510>'
       let ch = '<:voice:872428749642170378>'
-      
+
       const embed = new MessageEmbed()
       .setColor('#FFFFFF')
       .setThumbnail(message.guild.iconURL({ dynamic: true }))
       .addFields(
         { name: 'Name', value: `${message.guild.name}`},
-        { name: 'Owner', value: `${message.guild.ownerId}` },
-        { name: '<:djss:884719235681648651> Version', value: `Discord.js \`2.0.2\`` },
+        { name: 'Owner', value: `<@${message.guild.ownerId}>` },
+        { name: 'Version', value: `<:djss:884719235681648651> Discord.js \`2.0.2\`` },
       )
       .addFields(
         { name: 'Roles', value: `${roles.length}`},
     )
-      .addField('Channels', `${txt} Channels : ${message.guild.channels.cache.filter((c) => c.type === "text").size} 
-           ${ch} Voice Channels :  ${message.guild.channels.cache.filter((c) => c.type === "voice").size}`,
+      .addField('Channels', `${txt} Channels : ${message.guild.channels.cache.filter((c) => c.type === "GUILD_TEXT").size} 
+           ${ch} Voice Channels :  ${message.guild.channels.cache.filter((c) => c.type === "GUILD_VOICE").size}`,
      )
     .setFooter(`Requested By ${message.author.tag}`, message.author.displayAvatarURL())
     .setTimestamp();
