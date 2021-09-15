04a350dd9503d591d2495ff1816911c1a7668e7c
Kayz-xx <83003158+Kayz-xx@users.noreply.github.com>
2021-09-15 21:48:19 +0530

    Update economy.js

--- economy.js
+++ economy.js
@@ -133,10 +133,10 @@ module.exports.getDonation = async (guildId, userId) => {
 
 module.exports.getTotal = async (guildId) => {
   return await mongo().then(async (mongoose) => {
-  let arr = await profileSchema.find({guildId: guildId})
-let total = 0
-Promise.all(arr.map(a => total = parseInt(total) + parseInt(a.coins)))
-return total.toLocaleString()
+    let arr = await profileSchema.find({guildId: guildId})
+    let total = 0
+    Promise.all(arr.map(a=> total = parseInt(total.toString()) + parseInt(a.coins.toString())))
+    return total
     })
     }
 
