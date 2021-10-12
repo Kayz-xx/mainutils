d4e88cc8ac813f81c0f0a7bdf5d74eec84f76fd2
Kayz-xx <83003158+Kayz-xx@users.noreply.github.com>
2021-10-12 09:33:18 +0530

    Update itemcalc.js

--- commands/Donations/itemcalc.js
+++ commands/Donations/itemcalc.js
@@ -11,12 +11,110 @@ module.exports = {
 	description: 'Adds donation to a user',
 
 	async execute(client, message, cmd, args) {
-		let data5 =
-			(await db
-				.ref(`Donations/Info/${message.guild.id}/List`)
-				.once('value')
-				.then((snapshot) => snapshot.val())) || [];
-		db.ref(`Donations/Info/${message.guild.id}/List`);
+		function similarityBetween(s1, s2) {
+			let longer = s1;
+			let shorter = s2;
+			if (s1.length < s2.length) {
+			  longer = s2;
+			  shorter = s1;
+			}
+			const longerLength = longer.length;
+			if (longerLength === 0) {
+			  return 1.0;
+			}
+			return (
+			  (longerLength - editDistance(longer, shorter)) /
+			  parseFloat(longerLength)
+			);
+		  }
+		
+		  function editDistance(s1, s2) {
+			s1 = s1.toLowerCase();
+			s2 = s2.toLowerCase();
+		
+			const costs = [];
+			for (let i = 0; i <= s1.length; i++) {
+			  let lastValue = i;
+			  for (let j = 0; j <= s2.length; j++) {
+				if (i === 0) {
+				  costs[j] = j;
+				} else {
+				  if (j > 0) {
+					let newValue = costs[j - 1];
+					if (s1.charAt(i - 1) !== s2.charAt(j - 1)) {
+					  newValue =
+						Math.min(
+						  Math.min(newValue, lastValue),
+						  costs[j]
+						) + 1;
+					}
+					costs[j - 1] = lastValue;
+					lastValue = newValue;
+				  }
+				}
+			  }
+			  if (i > 0) {
+				costs[s2.length] = lastValue;
+			  }
+			}
+			return costs[s2.length];
+		  }
+		
+		  function search(query) {
+			query = query.toLowerCase();
+		
+			const target = items;
+			const candidates = [];
+		
+			for (const item in target) {
+			  const candidate = {
+				item: target[item],
+				similarity: 0,
+			  };
+		
+			  if (candidate.item.id.toLowerCase() === query) {
+				candidate.similarity = 1;
+			  } else if (candidate.item.name.toLowerCase() === query) {
+				candidate.similarity = 0.999;
+			  } else if (
+				candidate.item.name.toLowerCase().includes(" " + query + " ") ||
+				candidate.item.id.includes(" " + query + " ")
+			  ) {
+				candidate.similarity = 0.998;
+			  } else if (
+				candidate.item.name.toLowerCase().includes(query + " ") ||
+				candidate.item.id.includes(query + " ")
+			  ) {
+				candidate.similarity = 0.997;
+			  } else if (
+				candidate.item.name.toLowerCase().includes(" " + query) ||
+				candidate.item.id.includes(" " + query)
+			  ) {
+				candidate.similarity = 0.997;
+			  } else if (
+				candidate.item.name.toLowerCase().includes(query) ||
+				candidate.item.id.includes(query)
+			  ) {
+				candidate.similarity = 0.996;
+			  } else {
+				const similarity = similarityBetween(
+				  query,
+				  candidate.item.name
+				);
+				candidate.similarity = similarity;
+			  }
+		
+			  candidates.push(candidate);
+			}
+			return candidates.sort((a, b) => b.similarity - a.similarity)[0]
+		  
+		  }
+		
+		let items =
+		(await db
+		  .ref(`Donations/Info/${message.guild.id}/List`)
+		  .once('value')
+		  .then((snapshot) => snapshot.val())) || [];
 		let values = args.join(' ').split('+');
 		if (values.length < 1)
 			return message.reply('You must atleast specify 1 item and amount');
@@ -34,15 +132,13 @@ module.exports = {
 			if (!i[1]) {
 				break;
 			}
-			let item =
-				data5.find(
-					(item) => item.name.toUpperCase() === i[1].toUpperCase()
-				) ||
-				data5.find(
-					(item) => item.aliases.toUpperCase() === i[1].toUpperCase()
-				);
-			if (!item)
-				return message.reply({ content: `Could not find that item(${i[1]})!` });
+			let tem =
+			search(i[1])
+		  
+			if (tem.similarity <= 0.6)
+			return message.reply({ content: `Could not find that item(${i[1]})!` });
+		  
+		  	const item = tem.item
 			coins = coins + item.amount * Number(amount);
 		}
 
