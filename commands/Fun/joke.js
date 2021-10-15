3ccb7af8991caa9b1c281e42b6b5d18bbb7fc93d
Kayz-xx <83003158+Kayz-xx@users.noreply.github.com>
2021-10-15 16:57:19 +0530

    Update joke.js

--- commands/Fun/joke.js
+++ commands/Fun/joke.js
@@ -8,7 +8,7 @@ module.exports = {
   async execute(client, message, cmd,  args) {
 
     let data = await Random.getJoke()
-      message.channel.send({embeds: [data.embed]})
+     message.channel.send({embeds: [data.embed]})
     
   }
 }
