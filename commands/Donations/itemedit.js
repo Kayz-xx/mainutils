2a158e35e8e700e8fdf723277da6aab47bd60c0f
Kayz-xx <83003158+Kayz-xx@users.noreply.github.com>
2021-10-15 10:56:05 +0530

    Update itemedit.js

--- commands/Donations/itemedit.js
+++ commands/Donations/itemedit.js
@@ -1,45 +1,150 @@
-const { Client, Message, MessageEmbed, Collection } = require('discord.js')
+const { Client, Message, MessageEmbed, Collection, Permissions } = require('discord.js')
 const formatter = new Intl.NumberFormat('en')
 const {db} = require('../../firebase.js')
-const {Permissions} = require('discord.js')
+
 
 module.exports = {
     name: 'itemedit',
     aliases: [],
     cooldown: '0',
-    usage: '<name> <amount>',
+    usage: '<amount> <aliases> <name>',
     permissions: [],
     category: 'Donations',
     
     async execute(client, message, cmd,  args) {
-        if(!message.member.roles.cache.has("789854214288965644") && !message.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) return;
-        let name = args[0]
-        if(!name)
-        return message.channel.send({content: "Specify an item to edit."})
-        let amount = args[1]
+        function similarityBetween(s1, s2) {
+			let longer = s1;
+			let shorter = s2;
+			if (s1.length < s2.length) {
+			  longer = s2;
+			  shorter = s1;
+			}
+			const longerLength = longer.length;
+			if (longerLength === 0) {
+			  return 1.0;
+			}
+			return (
+			  (longerLength - editDistance(longer, shorter)) /
+			  parseFloat(longerLength)
+			);
+		  }
+		
+		  function editDistance(s1, s2) {
+			s1 = s1.toLowerCase();
+			s2 = s2.toLowerCase();
+		
+			const costs = [];
+			for (let i = 0; i <= s1.length; i++) {
+			  let lastValue = i;
+			  for (let j = 0; j <= s2.length; j++) {
+				if (i === 0) {
+				  costs[j] = j;
+				} else {
+				  if (j > 0) {
+					let newValue = costs[j - 1];
+					if (s1.charAt(i - 1) !== s2.charAt(j - 1)) {
+					  newValue =
+						Math.min(
+						  Math.min(newValue, lastValue),
+						  costs[j]
+						) + 1;
+					}
+					costs[j - 1] = lastValue;
+					lastValue = newValue;
+				  }
+				}
+			  }
+			  if (i > 0) {
+				costs[s2.length] = lastValue;
+			  }
+			}
+			return costs[s2.length];
+		  }
+		
+		  function search(query) {
+			query = query.toLowerCase();
+		
+			const target = items;
+			const candidates = [];
+		
+			for (const item in target) {
+			  const candidate = {
+				item: target[item],
+				similarity: 0,
+			  };
+		
+			  if (candidate.item.id.toLowerCase() === query) {
+				candidate.similarity = 1;
+			  } else if (candidate.item.name.toLowerCase() === query) {
+				candidate.similarity = 0.999;
+			  } else if (
+				candidate.item.name.toLowerCase().includes(" " + query + " ") ||
+				candidate.item.id.includes(" " + query + " ")
+			  ) {
+				candidate.similarity = 0.998;
+			  } else if (
+				candidate.item.name.toLowerCase().includes(query + " ") ||
+				candidate.item.id.includes(query + " ")
+			  ) {
+				candidate.similarity = 0.997;
+			  } else if (
+				candidate.item.name.toLowerCase().includes(" " + query) ||
+				candidate.item.id.includes(" " + query)
+			  ) {
+				candidate.similarity = 0.997;
+			  } else if (
+				candidate.item.name.toLowerCase().includes(query) ||
+				candidate.item.id.includes(query)
+			  ) {
+				candidate.similarity = 0.996;
+			  } else {
+				const similarity = similarityBetween(
+				  query,
+				  candidate.item.name
+				);
+				candidate.similarity = similarity;
+			  }
+		
+			  candidates.push(candidate);
+			}
+			return candidates.sort((a, b) => b.similarity - a.similarity)[0]
+		  
+		  }
+		
+        if(!message.member.roles.cache.has("764885367400693764") && !message.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) return;
+        let name = args.slice(2).join(" ")
+        if(!name) return message.reply({content: 'Please specify an item name'})
+        let amount = args[0]
         if(!amount)
-        return message.channel.send({content: "Specify an amount"})
-        let aliases = args[2]
+        return message.reply({content: 'Please specify an amount'})
+        let aliases = args[1]
         if(!aliases)
-        return message.channel.send({content: "Specify an alias"})
-        let data = await db
-        .ref(`Donations/Info/${message.guild.id}/List`)
-        .once("value")
-        .then(snapshot => snapshot.val())|| []
-        const found = data.find(item => item.name.toUpperCase() === name.toUpperCase()) || data.find(item => item.id.toUpperCase() === name.toUpperCase())
-        const place = data.indexOf(found)
+        return message.reply({content: 'Please specify item aliases'})
+        let items =
+        (await db
+          .ref(`Donations/Info/${message.guild.id}/List`)
+          .once('value')
+          .then((snapshot) => snapshot.val())) || [];
+          let tem =
+          search(name)
+            console.log(tem.similarity)
+          if (tem.similarity <= 0.6)
+          return message.reply({ content: `Could not find that item!` });
+
+        const found = tem.item
+        const place = items.indexOf(found)
         if(found) {
-            data[place] = {
+            items[place] = {
                 "name" : found.name,
-                "amount" : parseInt(amount),
+                "amount" : Number(amount),
                 "id": aliases,
                 "type" : found.type
               }
-            db.ref(`Donations/Info/${message.guild.id}/List`).set(data)
-            const item = data.find(item => item.name.toUpperCase() === name.toUpperCase()) || data.find(item => item.id.toUpperCase() === name.toUpperCase())
+            
+            db.ref(`Donations/Info/${message.guild.id}/List`).set(items)
+            let num = Number(amount)
             let embed6 = new MessageEmbed()
-            .setAuthor(`Item ${name} price set to: ${amount}`)
-            .setDescription(`Alias set to ${aliases}`)
+            .setDescription(`Item **${found.name}** price set to: \`${num.toLocaleString()}\`\n\nAlias set to: **${aliases}**`)
             .setColor("RANDOM")
             message.channel.send({embeds: [embed6]})
         } else {
