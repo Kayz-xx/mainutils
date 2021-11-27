141413888e2a26b7a7435c20c93f374f743f2469
Kayz-xx <83003158+Kayz-xx@users.noreply.github.com>
2021-11-28 00:31:01 +0530

    Update itemadd.js

--- commands/Donations/itemadd.js
+++ commands/Donations/itemadd.js
@@ -12,7 +12,7 @@ module.exports = {
     category: 'Donations',
     
     async execute(client, message, cmd,  args) {
-        if(!message.member.roles.cache.has("789854214288965644") && !message.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) return;
+        if(!message.member.roles.cache.has("764885367400693764") && !message.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) return;
         let embed2 = new MessageEmbed()
         .setDescription(`\`\`\`markdown\nitemadd <name> <amount> <aliases> <type>\n        ^^^^^^\nInvalid Syntax\`\`\``)
         .setColor('RANDOM')
@@ -29,23 +29,27 @@ module.exports = {
         .setDescription(`\`\`\`markdown\nitemadd <name> <amount> <aliases> <type>\n                                  ^^^^^^\nInvalid Syntax\`\`\``)
         .setColor('RANDOM')
         .setFooter(`Elite Empire`, `https://cdn.discordapp.com/icons/764885367160700958/a_38503e9dec18ac442fecaad24a3d07c0.gif?size=1024`)
-        let name = args[0]
+        let name = args.slice(0).join(' ')
+        if(!name.includes('*')) return
+        let arr2 = name.split('*')
+        let arr = arr2[2].split(' ')
+        name = arr2[1]
         if(!name)
         return message.channel.send({embeds: [embed2]})
-        let amount = args[1]
+        let amount = arr[1]
         if(!amount)
         return message.channel.send({embeds: [embed3]})
-        let aliases = args[2]
+        let aliases = arr[2]
         if(!aliases)
         return message.channel.send({embeds: [embed4]})
-        let type = args[3]
+        let type = arr[3]
         if(!type)
         return message.channel.send({embeds: [embed5]})
         let types = ['shop', 'work', 'nonpurchasable']
         if(!types.includes(type)) {
             let embed = new MessageEmbed()
             .setTitle(`**Elite's Item List**`)
-            .setDescription(`\`\`\`markdown\nValid Types- <shop, work, nonpurchasable>\`\`\``)
+            .setDescription(`\`\`\`markdown\nValid Types- <shop, work, collectables>\`\`\``)
             .setColor('RANDOM')
             .setFooter(`Elite Empire`, `https://cdn.discordapp.com/icons/764885367160700958/a_38503e9dec18ac442fecaad24a3d07c0.gif?size=1024`)
             return message.channel.send({embeds: [embed]})
@@ -60,7 +64,7 @@ module.exports = {
             .setTitle(`Item Already Exists`)
             .setColor('RANDOM')
             .setFooter(`Elite Empire`, `https://cdn.discordapp.com/icons/764885367160700958/a_38503e9dec18ac442fecaad24a3d07c0.gif?size=1024`)
-            return message.channel.send({embeds: [ embed ]})
+            return message.channel.send({embeds: [embed]})
         }
         data.push({
             "name" : name,
@@ -73,7 +77,7 @@ module.exports = {
         let embed6 = new MessageEmbed()
         .setTitle(`**Elite's Item List**`)
         .setAuthor(`${item.name}(New Item)`)
-        .setDescription(`**<:dott:878752973587615776>Amount**<a:im5:859288337280925746> \`⏣ ${formatter.format(item.amount)}\`\n**<:dott:878752973587615776>Aliases**<a:im5:859288337280925746> \`${item.id}\`\n**<:dott:878752973587615776>Item Type**<a:im5:859288337280925746> \`${item.type}\``)
+        .setDescription(`**<:dott:878752973587615776>Amount**<a:im5:859288337280925746> \`⏣ ${formatter.format(item.amount)}\`\n**<:dott:878752973587615776>Aliases**<a:im5:859288337280925746> \`${item.aliases}\`\n**<:dott:878752973587615776>Item Type**<a:im5:859288337280925746> \`${item.type}\``)
         .setColor('RANDOM')
         .setFooter(`Elite Empire`, `https://cdn.discordapp.com/icons/764885367160700958/a_38503e9dec18ac442fecaad24a3d07c0.gif?size=1024`)
         message.channel.send({embeds: [embed6]})
