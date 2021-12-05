125e6cfa05e3e323cb9715e35d58bab82839f041
Kayz-xx <83003158+Kayz-xx@users.noreply.github.com>
2021-12-05 12:49:57 +0530

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -123,39 +123,7 @@ try {
 		client.commands.get(cmd) ||
 		client.commands.find((a) => a.aliases && a.aliases.includes(cmd));
 
-	const validPermissions = [
-		'CREATE_INSTANT_INVITE',
-		'KICK_MEMBERS',
-		'BAN_MEMBERS',
-		'ADMINISTRATOR',
-		'MANAGE_CHANNELS',
-		'MANAGE_GUILD',
-		'ADD_REACTIONS',
-		'VIEW_AUDIT_LOG',
-		'PRIORITY_SPEAKER',
-		'STREAM',
-		'VIEW_CHANNEL',
-		'SEND_MESSAGES',
-		'SEND_TTS_MESSAGES',
-		'MANAGE_MESSAGES',
-		'EMBED_LINKS',
-		'ATTACH_FILES',
-		'READ_MESSAGE_HISTORY',
-		'MENTION_EVERYONE',
-		'USE_EXTERNAL_EMOJIS',
-		'VIEW_GUILD_INSIGHTS',
-		'CONNECT',
-		'SPEAK',
-		'MUTE_MEMBERS',
-		'DEAFEN_MEMBERS',
-		'MOVE_MEMBERS',
-		'USE_VAD',
-		'CHANGE_NICKNAME',
-		'MANAGE_NICKNAMES',
-		'MANAGE_ROLES',
-		'MANAGE_WEBHOOKS',
-		'MANAGE_EMOJIS',
-	];
+	const validPermissions = [];
 	if (command) {
 		if (!cooldowns.has(command.name)) {
 			cooldowns.set(command.name, new Discord.Collection());
