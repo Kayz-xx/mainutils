06566b0346e19006f7ac2729ad68919da72dcc52
Kayz-xx <83003158+Kayz-xx@users.noreply.github.com>
2021-12-05 12:48:16 +0530

    Update an.js

--- an.js
+++ an.js
@@ -1,15 +1,1 @@
 
-const mongo = require('./mongo')
-const profileSchema = require('./schemas/profile-schema')
-async function asyncCall() {
-return await mongo().then(async (mongoose) => {
-    console.log("Searching...")
-let arr = await profileSchema.find({guildId: '764885367160700958'})
-let newarr = arr.filter(x => x.coins >= 1)
-/*let total = 0
-Promise.all(arr.map(a=> total = parseInt(total.toString()) + parseInt(a.coins.toString())))*/
-console.log(newarr.length)
-})
-    }
-asyncCall()
-    
