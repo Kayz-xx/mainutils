11c05423fd84d1964078b96334f3bd0f253d993b
Kayz-xx <83003158+Kayz-xx@users.noreply.github.com>
2021-12-06 17:07:25 +0530

    Update slowmode.js

--- commands/Misc/slowmode.js
+++ commands/Misc/slowmode.js
@@ -10,7 +10,7 @@ module.exports = {
 	async execute(client, message, cmd, args) {
      try{
         if(!message.member.roles.cache.has(`792799102140022785`) && !message.member.roles.cache.has(`764885367400693764`) && !message.member.permissions.has(Permissions.FLAGS.MANAGE_MESSAGES)) return;
-        if(message.channel.id !== "792795803037990943" && message.channel.id !== "792505415748812801") return message.reply('Command can only be used in <#792795803037990943> or <#792505415748812801>')
+        if(message.channel.id !== "792795803037990943" && message.channel.id !== "792505415748812801" && message.channel.id !== '916771549753778246') return message.reply('Command can only be used in <#792795803037990943> or <#792505415748812801>')
         let time = args[0]
         if(!time) return message.channel.send("Please specify a time!")
         if(isNaN(time)) return message.reply('Please specify a valid number')
