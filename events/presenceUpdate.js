59ddb9ee681fe6ad1f896f147ff0e72e6868b694
Kayz-xx <83003158+Kayz-xx@users.noreply.github.com>
2021-12-11 10:01:47 +0530

    Update presenceUpdate.js

--- events/presenceUpdate.js
+++ events/presenceUpdate.js
@@ -1,16 +1,13 @@
   module.exports.run = async (client, oldPresence, newPresence) => {
   if(newPresence.guild == "764885367160700958") {
-  const role = newPresence.guild.roles.cache.get("872900600046690346");
-  const member = newPresence.member
-  const guild = newPresence.guild
+  const {member, guild} = newPresence
   if(newPresence.activities) {
   newPresence.activities.forEach((activity) => {
-      if(activity.name == "Custom Status") {
-          if(activity.state == null) return;
+      if(activity.name === "Custom Status") {
+          if(activity.state === null) return;
           let statuses = ['.gg/elites', 'discord.gg/elites'];
           if (statuses.some(x => activity.state.includes(x))) {
-      
-            return member.roles.add(role)
+            return member.roles.add('872900600046690346')
             } 
           }
        })
