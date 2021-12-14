968e75f95e82265673a886a8d49c9bd866c846cb
Kayz-xx <83003158+Kayz-xx@users.noreply.github.com>
2021-12-14 14:27:38 +0530

    Update profile.js

--- commands/Donations/profile.js
+++ commands/Donations/profile.js
@@ -6,7 +6,7 @@ const formatter = new Intl.NumberFormat('en')
 
 module.exports = {
     name: 'profile',
-    aliases: [],
+    aliases: ['wcheck', 'kcheck'],
     cooldown: '0',
     permissions: [],
     category: 'Donations',
@@ -14,28 +14,28 @@ module.exports = {
     description: 'Checks a users donation or your own donation!',
     
     async execute(client, message, cmd,  args) {
-    let str
-    let che = false
-    let user = args[0]
-    let target;
-    if(!user) target = message.author
-    else {
-    user = user.replace(/[\\<>@#&!]/g, "");
-    target = await client.users.fetch(user).catch(error => {})
-    if(!target) {
-        che = true
-        str = "This user was not found"
-        target = message.author
-    }
-    }
+        let str
+        let che = false
+        let user = args[0]
+        let target;
+        if(!user) target = message.author
+        else {
+        user = user.replace(/[\\<>@#&!]/g, "");
+        target = await client.users.fetch(user).catch(error => {})
+        if(!target) {
+            che = true
+            str = "This user was not found"
+            target = message.author
+        }
+        }
+    
+        const guildId = message.guild.id
+        const userId = target.id
 
-    const guildId = message.guild.id
-    const userId = target.id
-
-    const coins = await economy.getCoins(guildId, userId)
-    const coins2 = await economy2.getCoins(guildId, userId)
-    const coins3 = await economy3.getCoins(guildId, userId)
-
+        const coins = await economy.getCoins(guildId, userId)
+        const coins2 = await economy2.getCoins(guildId, userId)
+        const coins3 = await economy3.getCoins(guildId, userId)
+    if(cmd === 'profile') {
     let embed = new MessageEmbed()
     .setAuthor(`${target.tag}'s Donations`)
     .setColor("RANDOM")
@@ -47,5 +47,29 @@ module.exports = {
         embed.setFooter(str)
     }
     message.channel.send({embeds: [embed]})
+}
+    if(cmd === 'wcheck') {    
+        let embed = new MessageEmbed()
+        .setAuthor(`${target.tag}'s Donations`)
+        .setColor("RANDOM")
+        .addField(`Owo Donation`, `${formatter.format(coins)} **Cowoncy** <:cowoncy:919665528811974667>`)
+        .setTimestamp()
+        if(che) {
+            embed.setFooter(str)
+        }
+        message.channel.send({embeds: [embed]})
+    }
+
+    if(cmd === 'kcheck') {
+        let embed = new MessageEmbed()
+        .setAuthor(`${target.tag}'s Donations`)
+        .setColor("RANDOM")
+        .addField(`Karuta Donation`, `${formatter.format(coins2)} **Tickets** 🎟️`)
+        .setTimestamp()
+        if(che) {
+            embed.setFooter(str)
+        }
+        message.channel.send({embeds: [embed]})
+    }
   },
 }
