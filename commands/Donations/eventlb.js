0dede3fb6f34d1e096c91310b38c1d3bffd98965
Kayz-xx <83003158+Kayz-xx@users.noreply.github.com>
2021-12-14 23:32:04 +0530

    Update eventlb.js

--- commands/Donations/eventlb.js
+++ commands/Donations/eventlb.js
@@ -1,6 +1,8 @@
 
 const formatter = new Intl.NumberFormat('en')
 const eventdonations = require('../../eventdonations')
+const economy = require('../../owo')
+const economy2 = require('../../karuta')
 const {
 	Client,
 	Message,
@@ -13,7 +15,7 @@ const {
   let page = 0;
 module.exports = {
     name: 'eventleaderboard',
-    aliases: ['eventleaderboard', 'eventlb'],
+    aliases: ['eventleaderboard', 'eventlb', 'wleaderboard', 'kleaderboard'],
     cooldown: '0',
     usage: '',
     permissions: [],
@@ -22,7 +24,7 @@ module.exports = {
     description: 'Event Donation Leaderboard',
     
     async execute(client, message, cmd,  args) {
-
+      if(cmd === 'eventleaderboard' || cmd === 'eventlb') {
      
       const guildId = message.guild.id
       const data = await eventdonations.getDonation(guildId);
@@ -153,5 +155,268 @@ let pg = text.length - 1
       components: [rows2],
     });
   });
+  } 
+    if(cmd === 'wleaderboard') {
+      const guildId = message.guild.id
+      const data = await economy.getDonation(guildId);
+
+
+      let first = new MessageButton()
+      .setEmoji('<:fastb:878937208818630706>')
+      .setCustomId('first')
+      .setStyle('SECONDARY');
+
+    let back = new MessageButton()
+      .setEmoji('<:behind:875319719161397248>')
+      .setCustomId('back')
+      .setStyle('SECONDARY');
+
+    let next = new MessageButton()
+      .setEmoji('<:ahead:875319731220017162>')
+      .setCustomId('next')
+      .setStyle('SECONDARY');
+
+    let last = new MessageButton()
+      .setEmoji('<:fasta:878937199578607626>')
+      .setCustomId('last')
+      .setStyle('SECONDARY');
+
+    let del = new MessageButton()
+      .setEmoji('<:Cancel:875313311640616971>')
+      .setCustomId('del')
+      .setStyle('DANGER');
+
+    let row = new MessageActionRow().addComponents(
+      first,
+      back,
+      del,
+      next,
+      last,
+    );
+
+
+let text = []
+for(let i = 0; i < data.length; i++) {
+  const {userId, coins} = data[i];
+
+  text.push(`${i+1}) <@!${userId}> - **${formatter.format(coins)} cowoncy**\n`)
+}
+let pg = text.length - 1
+
+
+      const index = 10;
+      const generateEmbed = (start) => {
+      const current = text.slice(start, start + index).join(`\n\n`);
+      const embed = new MessageEmbed ()
+      .setTitle(`Donation Leaderboard in ${message.guild.name}`)
+      .setDescription(`${current}`)
+      .setFooter(`These are normal donations`)
+      .setColor('88FFF7')
+      .setFooter(
+        `Page ${start / 10} of ${Math.floor(pg / 10)}`
+      );
+      return embed;
+      };
+      
+ 
+  const msg = await message.channel.send({
+    embeds: [generateEmbed(0)],
+    components: [row],
+  });
+
+  const filter = (btn) => btn.user.id === message.author.id;
+
+  const collector = msg.createMessageComponentCollector({
+    filter,
+    time: 60000,
+  });
+
+  collector.on('collect', async (btn) => {
+    if (btn.customId === 'first') {
+      page = 0;
+      btn.update({
+        embeds: [generateEmbed(page)],
+        components: [row],
+      });
+    }
+    if (btn.customId === 'back') {
+      if (page > Math.floor(pg / 10) * 10 || page <= 0) {
+        return btn.deferUpdate();
+      } else {
+        page -= index;
+        btn.update({
+          embeds: [generateEmbed(page)],
+          components: [row],
+        });
+      }
+    }
+    if (btn.customId === 'next') {
+      if (page >= Math.floor(pg / 10) * 10 || page < 0) {
+        return btn.deferUpdate();
+      } else {
+        page += index;
+        btn.update({
+          embeds: [generateEmbed(page)],
+          components: [row],
+        });
+      }
+    }
+    if (btn.customId === 'last') {
+      page = Math.floor(pg / 10) * 10;
+      btn.update({
+        embeds: [generateEmbed(page)],
+        components: [row],
+      });
+    }
+    if (btn.customId === 'del') {
+      btn.deferUpdate();
+      collector.stop();
+      setTimeout(() => msg.delete(), 100);
+    }
+  });
+  collector.on('end', (reason) => {
+    msg.components[0].components.forEach((com) => {
+      com.setDisabled(true);
+      com.setStyle('SECONDARY');
+    });
+    let rows2 = new MessageActionRow().addComponents(
+      msg.components[0].components
+    );
+    msg.edit({
+      components: [rows2],
+    });
+  });
+    }
+    if(cmd === 'kleaderboard') {
+      const guildId = message.guild.id
+      const data = await economy2.getDonation(guildId);
+
+
+      let first = new MessageButton()
+      .setEmoji('<:fastb:878937208818630706>')
+      .setCustomId('first')
+      .setStyle('SECONDARY');
+
+    let back = new MessageButton()
+      .setEmoji('<:behind:875319719161397248>')
+      .setCustomId('back')
+      .setStyle('SECONDARY');
+
+    let next = new MessageButton()
+      .setEmoji('<:ahead:875319731220017162>')
+      .setCustomId('next')
+      .setStyle('SECONDARY');
+
+    let last = new MessageButton()
+      .setEmoji('<:fasta:878937199578607626>')
+      .setCustomId('last')
+      .setStyle('SECONDARY');
+
+    let del = new MessageButton()
+      .setEmoji('<:Cancel:875313311640616971>')
+      .setCustomId('del')
+      .setStyle('DANGER');
+
+    let row = new MessageActionRow().addComponents(
+      first,
+      back,
+      del,
+      next,
+      last,
+    );
+
+
+let text = []
+for(let i = 0; i < data.length; i++) {
+  const {userId, coins} = data[i];
+
+  text.push(`${i+1}) <@!${userId}> - **${formatter.format(coins)} tickets**\n`)
+}
+let pg = text.length - 1
+
+
+      const index = 10;
+      const generateEmbed = (start) => {
+      const current = text.slice(start, start + index).join(`\n\n`);
+      const embed = new MessageEmbed ()
+      .setTitle(`Donation Leaderboard in ${message.guild.name}`)
+      .setDescription(`${current}`)
+      .setFooter(`These are normal donations`)
+      .setColor('88FFF7')
+      .setFooter(
+        `Page ${start / 10} of ${Math.floor(pg / 10)}`
+      );
+      return embed;
+      };
+      
+ 
+  const msg = await message.channel.send({
+    embeds: [generateEmbed(0)],
+    components: [row],
+  });
+
+  const filter = (btn) => btn.user.id === message.author.id;
+
+  const collector = msg.createMessageComponentCollector({
+    filter,
+    time: 60000,
+  });
+
+  collector.on('collect', async (btn) => {
+    if (btn.customId === 'first') {
+      page = 0;
+      btn.update({
+        embeds: [generateEmbed(page)],
+        components: [row],
+      });
+    }
+    if (btn.customId === 'back') {
+      if (page > Math.floor(pg / 10) * 10 || page <= 0) {
+        return btn.deferUpdate();
+      } else {
+        page -= index;
+        btn.update({
+          embeds: [generateEmbed(page)],
+          components: [row],
+        });
+      }
+    }
+    if (btn.customId === 'next') {
+      if (page >= Math.floor(pg / 10) * 10 || page < 0) {
+        return btn.deferUpdate();
+      } else {
+        page += index;
+        btn.update({
+          embeds: [generateEmbed(page)],
+          components: [row],
+        });
+      }
+    }
+    if (btn.customId === 'last') {
+      page = Math.floor(pg / 10) * 10;
+      btn.update({
+        embeds: [generateEmbed(page)],
+        components: [row],
+      });
+    }
+    if (btn.customId === 'del') {
+      btn.deferUpdate();
+      collector.stop();
+      setTimeout(() => msg.delete(), 100);
+    }
+  });
+  collector.on('end', (reason) => {
+    msg.components[0].components.forEach((com) => {
+      com.setDisabled(true);
+      com.setStyle('SECONDARY');
+    });
+    let rows2 = new MessageActionRow().addComponents(
+      msg.components[0].components
+    );
+    msg.edit({
+      components: [rows2],
+    });
+  });
+    }
     }
 }
