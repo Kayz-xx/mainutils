c4902f28e190a158b45bc2a147dd36793a090deb
Kayz-xx <83003158+Kayz-xx@users.noreply.github.com>
2022-01-17 16:30:10 +0530

    Update presenceUpdate.js

--- events/presenceUpdate.js
+++ events/presenceUpdate.js
@@ -6,6 +6,7 @@
       if(activity.name === "Custom Status") {
           if(activity.state === null) return;
           let statuses = ['.gg/elites', 'discord.gg/elites'];
+          if(activity.state.match(/discord.gg|.gg/g).length > 1) return;
           if (statuses.some(x => activity.state.includes(x))) {
             return member.roles.add('872900600046690346')
             } 
