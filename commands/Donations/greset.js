4125cf2825e9111212441e860b922440ab9073a9
Kayz-xx <83003158+Kayz-xx@users.noreply.github.com>
2022-01-26 22:40:11 +0530

    Create greset.js

---
+++ commands/Donations/greset.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+const {db} = require('../../firebase.js')
+module.exports = {
+	name: 'greset',
+	aliases: ['grinderreset'],
+	cooldown: '0',
+	usage: '',
+	permissions: [],
+	category: 'Donations',
+	description: 'Resets Weekly Donation',
+
+	async execute(client, message, cmd, args) {
+        if (!message.member.permissions.has(Discord.Permissions.FLAGS.ADMINISTRATOR)) return;
+        db.ref(`Grinders/${message.guild.id}`).set({})
+        let embed = new Discord.MessageEmbed()
+        .setColor('RANDOM')
+        .setDescription('Weekly Grinder Donations have been reset!')
+        message.channel.send({embeds: [embed]})
+	},
+};
