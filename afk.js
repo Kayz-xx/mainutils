9a41d6c387205648713cb0fce22df881fe1d8cdc
Kayz-xx <83003158+Kayz-xx@users.noreply.github.com>
2022-02-15 18:34:57 +0530

    Add files via upload

---
+++ afk.js
@@ -0,0 +1,32 @@
+const mongo = require('./mongo')
+const schema = require('./schemas/afk-schema')
+
+module.exports.find = async (userId, guildId) => {
+    return await mongo().then(async (mongoose) => {
+        const result = await schema.findOne({userId, guildId})
+        return result
+    })
+  }
+
+
+module.exports.create = async (userId, guildId, AFK, reason, timestamp) => {
+    return await mongo().then(async (mongoose) => {
+        const result = await schema.findOneAndUpdate({userId, guildId}, {userId, guildId, AFK, reason, timestamp}, {upsert: true, new: true})
+        return result
+    })
+  }
+
+
+module.exports.push = async (userId, guildId, url, author, time) => {
+    return await mongo().then(async (mongoose) => {
+        const result = await schema.findOneAndUpdate({userId, guildId}, {$push: {pings: {url, author, time}}}, {upsert: true, new: true})
+        return result
+    })
+  }
+
+  module.exports.set = async (userId, guildId) => {
+    return await mongo().then(async (mongoose) => {
+        const result = await schema.findOneAndUpdate({userId, guildId}, {AFK: false, pings: []}, {upsert: true, new: true})
+        return result
+    })
+  }
