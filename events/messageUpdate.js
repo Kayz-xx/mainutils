5ff3d80c18511845de38e707e1abac4f506a2c4c
Kayz-xx <83003158+Kayz-xx@users.noreply.github.com>
2022-02-18 10:09:12 +0530

    Update messageUpdate.js

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -1,8 +1,10 @@
 
 module.exports.run = (client, oldMessage, newMessage) => {
-
+    if(newMessage.author.bot) return;
     let snipes = client.snipes.get(newMessage.channel.id) || []
-
+    if(snipes.size > 100 || snipes.length > 100) {
+        snipes = snipes.slice(0, 50)
+    }
     snipes.unshift({
         msg: newMessage,
         image: newMessage.attachments.first()?.proxyURL || null,
