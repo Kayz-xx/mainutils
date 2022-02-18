78185b5db27ab68522f28723c209bb2a6e96e59c
Kayz-xx <83003158+Kayz-xx@users.noreply.github.com>
2022-02-18 12:43:02 +0530

    Update messageUpdate.js

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -1,6 +1,6 @@
 
 module.exports.run = (client, oldMessage, newMessage) => {
-    if(newMessage.author.bot) return;
+//     if(newMessage.author.bot) return;
     let snipes = client.snipes.get(newMessage.channel.id) || []
     if(snipes.size > 100 || snipes.length > 100) {
         snipes = snipes.slice(0, 50)
