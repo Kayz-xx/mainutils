8871fc43e7e06f541f73bc02f3da7b3bc54b6a4d
Kayz-xx <83003158+Kayz-xx@users.noreply.github.com>
2022-02-22 09:55:34 +0530

    Create eping.js

---
+++ commands/Misc/eping.js
@@ -0,0 +1,16 @@
+const {Permissions, MessageEmbed} = require("discord.js")
+module.exports = {
+    name: 'eping',
+    aliases: [],
+    cooldown: 540,
+    category: 'Misc',
+
+    async execute(client, message, cmd,  args) {
+          if(message.guild.id === "764885367160700958") {
+          if(!message.member.roles.cache.some(x => x.id === '792799102140022785') && !message.member.roles.cache.some(x => x.id === '764885367400693764') && !message.member.permissions.has(Permissions.FLAGS.MANAGE_MESSAGES)) return 
+        setTimeout(() => message.delete(), 100)
+        const msg = args.slice(0).join(' ');
+        message.channel.send({content:`<@&778355810043559976> ${msg} **- ${message.author.tag}**`})
+        } 
+  }
+}	  
