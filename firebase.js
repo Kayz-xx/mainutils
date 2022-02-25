d067b61be3c24c840f9fff3211834dd369b420bd
Kayz-xx <kaydenmoraes@gmail.com>
2022-02-25 21:59:37 +0530

    Secure 2.0

--- firebase.js
+++ firebase.js
@@ -6,7 +6,7 @@ const config = {
   "type": "service_account",
   "project_id": process.env.PROJECT_ID,
   "private_key_id": process.env.PRIVATE_KEY_ID,
-  "private_key": process.env.PRIVATE_KEY,
+  "private_key": process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
   "client_email": "firebase-adminsdk-o0cli@discord-beta-b3add.iam.gserviceaccount.com",
   "client_id": "106644098161125900881",
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
