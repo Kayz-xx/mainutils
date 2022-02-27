b44c24abf25dcfd5d547b02a2417567ff7643e52
Kayz-xx <83003158+Kayz-xx@users.noreply.github.com>
2022-02-27 19:55:49 +0530

    Update donate.js

--- commands/Misc/donate.js
+++ commands/Misc/donate.js
@@ -103,7 +103,7 @@ const filter = (btn) => message.guild.members.cache.find((member) => member.id =
 }
 module.exports = {
     name: 'donate',
-    aliases: ['donate', 'eventdono', 'heistdono', 'wdonate'],
+    aliases: ['donate', 'eventdono', 'heistdono', 'wdonate', 'edonate'],
     cooldown: '0',
     permissions: [],
     usage: '<prize> <time> <winners> <requirement> <message>, eventdono- <event> <winners> <prize> <req> <message>',
@@ -328,5 +328,79 @@ module.exports = {
           console.log(error)
         }
 }  
+ if(cmd === 'wdonate') {
+      try {
+        await donate('872376861018177547', message, args, '913178075808817202')
+        } catch (error) {
+          console.log(error)
+        }
+}   if (cmd === 'edonate') {
+  async function eventDonation(role, message, args, channel) {
+    if(message.channel.id !== channel) return;
+    if(!message.member.roles.cache.has('764885367160700960')) return;
+    setTimeout(() => message.delete(), 100)
+    const prize = args.slice(0).join(' ');
+    if(!prize) return message.channel.send("Please specify a prize")
+  
+    let embed = new MessageEmbed()
+    .setAuthor('50k Event Donations', message.author.avatarURL({ dynamic:true }))
+    .setTitle(`${message.author.tag} wants to donate ${prize} <a:im4:858370157890371595>`)
+    .setThumbnail('https://cdn.discordapp.com/icons/764885367160700958/a_38503e9dec18ac442fecaad24a3d07c0.gif?size=1024')
+    .setColor('5C33F6')
+    .setFooter(`Thanks for your donation ${message.author.tag}`)
+    .setTimestamp()
+    let del = new MessageButton()
+    .setLabel("Claim")
+    .setCustomId('del')
+    .setStyle('SUCCESS');
+    let nul = new MessageButton()
+    .setLabel("Decline")
+    .setCustomId('nul')
+    .setStyle('DANGER');
+  
+  let row = new MessageActionRow().addComponents(del, nul);
+  
+  let msg = await message.channel.send({content: `<@&${role}>`, embeds: [embed], components: [row]})
+              
+  const filter = (btn) => message.guild.members.cache.find((member) => member.id === btn.user.id).roles.cache.some(x => x.id === role) || message.guild.members.cache.find((member) => member.id === btn.user.id).roles.cache.some(x => x.id === '764885367400693764') || message.guild.members.cache.find((member) => member.id === btn.user.id).permissions.has(Permissions.FLAGS.MANAGE_MESSAGES)
+  
+  
+    const collector = msg.createMessageComponentCollector({filter,
+      time: 600000,
+    });
+  
+    collector.on('collect', async (btn) => {
+    btn.deferUpdate()
+    btn.message.components[0].components.forEach((com) => {
+          com.setDisabled(true);
+          com.setStyle('SECONDARY');
+        });
+    let rows = new MessageActionRow().addComponents(btn.message.components[0].components)
+      if (btn.customId === 'del') {
+        btn.channel.send(`<@${btn.user.id}> has **claimed** your donation! Make sure to send items/cash to them.`)
+        msg.edit({
+          embeds: [embed],
+          components: [rows],
+        });
+        collector.stop();
+      }
+      if (btn.customId === 'nul') {
+        btn.channel.send(`<@${btn.user.id}> has **declined** your donation.`)
+        msg.edit({components: [rows]})
+        collector.stop();
+      }
+    })
+    collector.on('end', async (collected, reason) => {
+        msg.components[0].components.forEach((com) => {
+          com.setDisabled(true);
+          com.setStyle('SECONDARY');
+        });
+        let rows2 = new MessageActionRow().addComponents(msg.components[0].components)
+        msg.edit({content: `No one **claimed** the donation proceed **manually**.`, components: [rows2]});
+        message.channel.send({content: `<@&${role}>`})
+    })
+  }
+ eventDonation('764885367400693764', message, args, '942321197155254292')
+}
   }
 }
