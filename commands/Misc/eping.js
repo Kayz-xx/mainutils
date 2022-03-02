09a99a5e9e46e944fbd16a3ad65b2d4fd643f388
Kayz-xx <kaydenmoraes@gmail.com>
2022-03-02 09:29:24 +0530

    Ping Channels

--- commands/Misc/eping.js
+++ commands/Misc/eping.js
@@ -7,6 +7,7 @@ module.exports = {
 
     async execute(client, message, cmd,  args) {
           if(message.guild.id === "764885367160700958") {
+          if(message.channel.id !== '792795803037990943') return message.channel.send('You can only use this command in event channels.');
           if(!message.member.roles.cache.some(x => x.id === '792799102140022785') && !message.member.roles.cache.some(x => x.id === '764885367400693764') && !message.member.permissions.has(Permissions.FLAGS.MANAGE_MESSAGES)) return 
         setTimeout(() => message.delete(), 100)
         const msg = args.slice(0).join(' ');
--- commands/Misc/giveaway.js
+++ commands/Misc/giveaway.js
@@ -7,6 +7,8 @@ module.exports = {
 
     async execute(client, message, cmd,  args) {
         if(message.guild.id === "764885367160700958") {
+          let channels = ['764885369384599563', '764885369384599562', '793597603866214400', '831238263406198784', '764885369384599564']
+          if(!channels.some(channel => channel === message.channel.id)) return message.channel.send("You can only use this command in the giveaway channels.")
       if(!message.member.roles.cache.some(x => x.id === '768129052623372348') && !message.member.roles.cache.some(x => x.id === '764885367400693764') && !message.member.permissions.has(Permissions.FLAGS.MANAGE_MESSAGES)) return 
     setTimeout(() => message.delete(), 101)
       const msg = args.slice(0).join(' ');
